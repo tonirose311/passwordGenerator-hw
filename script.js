242b5af5770cd1085b15b398abdce8ab16249b5d
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-09-27 23:11:30 -0500

    fixed syntax error preventing prompt to come up

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var bigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var smallLetters = "abcdefghijklmnopqrstuvwxyz";
-// var numbers = "1234567890";
-// var specialCharacters = "!@#$%^&*()_-=+";
 
 // Write password to the #password input
 function writePassword() {
@@ -12,28 +8,22 @@ function writePassword() {
 
   passwordText.value = password;
 
-
-
-
   
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-//prompt for length of password
-var passLength = prompt("How long do you want your password to be? (8-128)");
-  while (passLength < 8 || passLength > 128) {
-    alert("Please choose a number between 8 and 128");
-    return;
+//prompt for length 
+  function generatePassword(){
+    var passLength = prompt("How long do you want your password to be? (8-128)");
+
+    //confirm what you want to include in password
+    var numbers = confirm("Do you want to include *Numbers* ?");
+    var bigLetters = confirm("Do you want to include *Uppercase Letters* ?");
+    var smallLetters = confirm("Do you want to include *Lowercase Letters* ?");
+    var specialCharacters = confirm("Do you want to include *Special Characters* ?");
   }
 
-
-
-//confirms for password criteria: Uppercase, Lowercase, Numbers, Special Characters
-var bigLetters = confirm ("Do you want to include *Uppercase Letters* ?");
-var smallLetters = confirm ("Do you want to include *Lowercase Letters* ?");
-var specialCharacters = confirm ("Do you want to include *Special Characters* ?");
-var numbers = confirm ("Do you want to include *Numbers* ?");
 
 
 
