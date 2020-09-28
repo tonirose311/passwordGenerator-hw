22821aa088233d4bf885200305b52f690f4ad7cf
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-09-27 21:17:59 -0500

    added more variables for password values

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var randomLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+"
+var bigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var smallLetters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890"
+var specialCharacters = "!@#$%^&*()_-=+"
 
 // Write password to the #password input
 function writePassword() {
