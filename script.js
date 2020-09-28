8534aed9907179181cb56a41d527b459212f736c
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-09-27 21:15:25 -0500

    removed for loop

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
   
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+}
